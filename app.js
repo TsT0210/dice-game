309f1109a9ff7bb585c613c06e7fae44baa4b52f
Tst <begz0210@gmail.com>
2020-01-23 19:35:28 +0800

    Toglogchiin eeljiig solidog bolgono.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлж хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -24,5 +24,33 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   diceDom.style.display = 'block';
   diceDom.src = 'dice-' + diceNumber + ".png";
+
+  // Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+    if(diceNumber !== 1){
+      roundScore = roundScore + diceNumber;
+      document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }else{
+
+      roundScore = 0;
+
+      document.getElementById('current-' + activePlayer).textContent = 0;
+
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+      // Улаан цэгийг шилжүүлэх код
+      document.querySelector(".player-0-panel").classList.toggle("active");
+      document.querySelector('.player-1-panel').classList.toggle("active");
+
+      diceDom.style.display = "none";
+
+      
+      // if(activePlayer === 0){
+      //   activePlayer = 1;
+      // }
+      // else{
+      //   activePlayer =0;
+      // }
+    }
+
 
 });
